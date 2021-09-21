085ee235ed277b83198e71581ec5737b8feb9bf1
hanishkvc <hanishkvc@gmail.com>
2021-09-22 02:19:44 +0530

    Add a task which continuously updates camera

--- landscape.py
+++ landscape.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 
-gSB = ShowBase()
-gGnd = gSB.loader.loadModel("models/environment")
-gGnd.reparentTo(gSB.render)
 
-gSB.run()
+def move_camera(task):
+    g['CamX'] += 0.1
+    g['SB'].camera.setPos(g['CamX'], g['CamY'], g['CamZ'])
+    g['SB'].camera.setHpr(0, 0, 0)
+    return Task.cont
+
+
+def init(g):
+    g['SB'] = ShowBase()
+    g['Gnd'] = g['SB'].loader.loadModel("models/environment")
+    g['Gnd'].reparentTo(g['SB'].render)
+    g['CamX'] = 0
+    g['CamY'] = 0
+    g['CamZ'] = 1
+
+
+def prepare(g):
+    g['SB'].taskMgr.add(move_camera, 'MoveCameraTsk')
+
+
+g = {}
+init(g)
+prepare(g)
+g['SB'].run()
 
