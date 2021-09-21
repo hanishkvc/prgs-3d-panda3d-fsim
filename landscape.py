5d9bb04cc55b29d8e816b4af8a6bdb4f046308f6
hanishkvc <hanishkvc@gmail.com>
2021-09-22 03:10:14 +0530

    Try set the camera height at beginning

--- landscape.py
+++ landscape.py
@@ -5,11 +5,15 @@ from direct.task import Task
 from panda3d.core import GeoMipTerrain
 
 
+def set_camera(g):
+    g['SB'].camera.setPos(g['CamX'], g['CamY'], g['CamZ'])
+    g['SB'].camera.setHpr(0, 0, 0)
+
+
 def move_camera(task):
     if (task.frame%24) == 0:
         g['CamX'] += 0.1
-    g['SB'].camera.setPos(g['CamX'], g['CamY'], g['CamZ'])
-    g['SB'].camera.setHpr(0, 0, 0)
+    set_camera(g)
     return Task.cont
 
 
@@ -21,12 +25,17 @@ def init(g):
     else:
         g['Gnd'] = GeoMipTerrain("Gnd")
         g['Gnd'].setHeightfield("./gnd_hm.jpg")
+        g['Gnd'].setBlockSize(32)
+        g['Gnd'].setNear(40)
+        g['Gnd'].setFar(100)
+        g['Gnd'].setFocalPoint(g['SB'].camera)
         g['Gnd'].getRoot().reparentTo(g['SB'].render)
         g['Gnd'].getRoot().setSz(100)
         g['Gnd'].generate()
     g['CamX'] = 0
     g['CamY'] = 0
-    g['CamZ'] = 1
+    g['CamZ'] = 250
+    set_camera(g)
 
 
 def prepare(g):
