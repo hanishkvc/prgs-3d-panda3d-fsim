636c6c9688878a31ef850d7657b54e8f966b623a
hanishkvc <hanishkvc@gmail.com>
2021-09-23 16:43:50 +0530

    Convert to class based logic roughly

--- landscape.py
+++ landscape.py
@@ -5,68 +5,72 @@ from direct.task import Task
 from panda3d.core import GeoMipTerrain, PNMImage
 
 
-def set_camera(g):
-    g['SB'].camera.setPos(g['CamX'], g['CamY'], g['CamZ'])
-    g['SB'].camera.setHpr(0, 0, 0)
+class FSim(ShowBase):
+
+    def __init__(self):
+        # Camera is the Main Actor for now
+        self.dX = 0
+        self.dY = 0.1
+        self.dZ = 0
+        self.gndWidth = 129
+        self.gndHeight = 129
+        self.create_terrain()
+        self.camera.setPos(0, 0, 5)
+        self.camera.setHpr(0, 0, 0)
 
 
-def move_camera(task):
-    if (task.frame%24) == 0:
-        g['CamX'] += 0.1
-    set_camera(g)
-    return Task.cont
-
-
-def update_gnd(task):
-    g['Gnd'].update()
-    return Task.cont
-
-
-def init(g):
-    g['SB'] = ShowBase()
-    if False:
-        g['Gnd'] = g['SB'].loader.loadModel("models/environment")
-        g['Gnd'].reparentTo(g['SB'].render)
-    else:
-        g['Gnd'] = GeoMipTerrain("Gnd")
-        g['Gnd'].setHeightfield("./gnd_hm.jpg")
-        g['Gnd'].setBlockSize(32)
-        g['Gnd'].setNear(40)
-        g['Gnd'].setFar(100)
-        g['Gnd'].setFocalPoint(g['SB'].camera)
-        g['Gnd'].getRoot().reparentTo(g['SB'].render)
-        g['Gnd'].getRoot().setSz(100)
-        g['Gnd'].generate()
-        hm = g['Gnd'].heightfield()
-        print("DBUG:Terrain:HM:{}x{}".format(hm.getXSize(), hm.getYSize()))
-        cm = PNMImage(hm.getXSize(), hm.getYSize())
-        g['Gnd'].setColorMap(cm)
-        cm = g['Gnd'].colorMap()
-        print("DBUG:Terrain:CM:{}x{}".format(cm.getXSize(), cm.getYSize()))
-        for x in range(hm.getXSize()):
-            for y in range(hm.getYSize()):
-                hv=hm.getGrayVal(x,y)
-                if hv < 25:
-                    cm.setBlue(x,y,1)
-                elif hv > 75:
-                    cm.setRed(x,y,1)
+    def create_terrain(self):
+        self.terrain = GeoMipTerrain("Gnd")
+        #self.terrain.setHeightField("./gnd_hf.png")
+        hf = PNMImage(self.gndWidth, self.gndHeight, PNMImage.CTGrayscale)
+        self.terrain.setHeightfield(hf)
+        print("DBUG:Terrain:HF:{}x{}".format(hf.getXSize(), hf.getYSize()))
+        # Setup a height map
+        for x in range(hf.getXSize()):
+            for y in range(hf.getYSize()):
+                if x < hf.getXSize()/3:
+                    hf.setGray(x, y, 0)
+                elif x < (hf.getXSize()*2/3):
+                    hf.setGray(x, y, 0.5)
                 else:
-                    cm.setGreen(x,y,1)
+                    hf.setGray(x, y, 1)
+        # Color the terrain based on height
+        cm = PNMImage(hf.getXSize(), hf.getYSize())
+        self.terrain.setColorMap(cm)
+        print("DBUG:Terrain:CM:{}x{}".format(cm.getXSize(), cm.getYSize()))
+        for x in range(hf.getXSize()):
+            for y in range(hf.getYSize()):
+                hfv = hf.getGray(x, y)
+                if hfv < 0.25:
+                    cm.setBlue(x, y, 1)
+                elif hfv > 0.75:
+                    cm.setRed(x, y, 1)
+                else:
+                    cm.setGreen(x, y, 1)
+        self.terrain.setBlockSize(32)
+        self.terrain.setNear(5)
+        self.terrain.setFar(50)
+        self.terrain.setFocalPoint(self.camera)
+        tRoot = self.terrain.getRoot()
+        tRoot.setSz(100)
+        tRoot.reparentTo(self.render)
+        self.terrain.generate()
 
-    g['CamX'] = 0
-    g['CamY'] = 0
-    g['CamZ'] = 250
-    set_camera(g)
+
+        def update(self, task):
+            x = self.camera.getX()*self.dX
+            y = self.camera.getY()*self.dY
+            z = self.camera.getZ()*self.dZ
+            self.camera.setPos(x, y, z)
+            return Task.cont
 
 
-def prepare(g):
-    g['SB'].useDrive()
-    #g['SB'].taskMgr.add(move_camera, 'MoveCameraTsk')
-    g['SB'].taskMgr.add(update_gnd, 'UpdateGnd')
+        def prepare(self):
+            #self.useDrive()
+            self.taskMgr.add(update, 'UpdateFSim')
 
 
-g = {}
-init(g)
-prepare(g)
-g['SB'].run()
+fsim = FSim()
+fsim.prepare()
+fsim.run()
 
