317870d3164d7fc44bb4a9ee81d2737b6e281165
hanishkvc <hanishkvc@gmail.com>
2021-09-24 17:39:02 +0530

    using Terrains root, also doesnt help with getPos of camera
    How do I get absolute position of camera in the scene graph???
    
    Tried using setFluidPos (which tracks for any inbetween things,
    so as to trigger collision etc) just in case. But didnt help,
    rather this was expected at one level.

--- fsim.py
+++ fsim.py
@@ -68,10 +68,10 @@ class FSim(ShowBase):
     def update(self, task):
         if (task.frame%24) == 0:
             print("DBUG:Update:240:Camera:{}:Trans:{}".format(self.camera.getPos(), self.ctrans))
-            print("DBUG:Update:241:Camera:{}:Rotat:{}".format(self.camera.getPos(self.render), self.crot))
+            print("DBUG:Update:241:Camera:{}:Rotat:{}".format(self.camera.getPos(self.terrain.getRoot()), self.crot))
             self.terrain.update()
         self.cpos += self.ctrans
-        self.camera.setPos(self.cpos)
+        self.camera.setFluidPos(self.cpos)
         return Task.cont
 
 
