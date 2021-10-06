8f5fa207e5611682296cdc3bc7524ee50a6f1379
hanishkvc <hanishkvc@gmail.com>
2021-10-07 03:22:22 +0530

    ImgUtilsPrg: Support generation of Panda3d compatible heightfield

--- utils/hkvc_imgutils.py
+++ utils/hkvc_imgutils.py
@@ -6,6 +6,9 @@
 #   This can be used to color say a heightfield image based on World Vegetation NDVI data
 # Option2: Reduce the color resolution / shades in a image
 #   This can be used to reduce the amount of detail in a heightfield to some extent.
+# Option3: Generate a Panda3D compatible heightfield png, from given heightfield image.
+#   One could give a GeoTiff image containing elevation as input for example.
+#   Note: The logic accepts elevation/heightfield/grayscale data in any file format.
 # HanishKVC, 2021
 # GPL
 #
@@ -34,16 +37,27 @@ def run_reduceshades():
     iu.save_rimg(fnRS, i2)
 
 
+def run_p3dhf():
+    iI = iu.load_rimg(gCfg['sFNameSrc'])
+    iA = iu.amplify_shades_rimg(iI)
+    iR = iu.resize_pwrof2square_rimg(iA)
+    fnHF = "{}.hf.png".format(gCfg['sFNameSrc'])
+    iu.save_rimg(fnHF, iR)
+
+
 def run_main():
     try:
         if gCfg['sCmd'] == "gray2color":
             run_gray2color()
         elif gCfg['sCmd'] == "reduceshades":
             run_reduceshades()
+        elif gCfg['sCmd'] == "p3dhf":
+            run_p3dhf()
     except:
         print(sys.exc_info())
         print("thisPrg --sCmd gray2color --sFNameSrc <srcImage> --sFNameRef <refImage>")
         print("thisPrg --sCmd reduceshades --sFNameSrc <srcImage>")
+        print("thisPrg --sCmd p3dhf --sFNameSrc <srcImage>")
 
 
 if __name__ == "__main__":
