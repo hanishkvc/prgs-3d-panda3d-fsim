9a7dd55819e53140f9f0d47d2d5604d63c6bcb68
hanishkvc <hanishkvc@gmail.com>
2021-10-12 00:09:57 +0530

    Utils:AptDat: Skeleton to save apt data into a db

--- utils/hkvc-apt.py
+++ utils/hkvc-apt.py
@@ -27,18 +27,21 @@ def cleanup_line(sL):
     return la
 
 
-def parse_aptdat(sFName):
+def parse_aptdat(sFName, db):
     f = open(sFName, errors='replace')
     state = '0'
     lCnt = 0
+    apt = {}
     for l in f:
         lCnt += 1
         l = l.strip()
         la = cleanup_line(l)
         if len(la) == 0:
             state = '0'
-            lat = None
-            lon = None
+            if apt.get('lat'):
+                k = "{:6.2f}-{:6.2f}".format(float(apt['lat']),float(apt['lon']))
+                db[k] = apt
+            apt = {}
             continue
         else:
             if gbDebug:
@@ -48,15 +51,23 @@ def parse_aptdat(sFName):
                 print("ERRR:ParseAptDat:{}:New Airport Line before ending of prev airport data?".format(lCnt))
                 continue
             state = 'A'
-            print("INFO:A:", la[4], la[5:])
+            apt['icao'] = la[4]
+            if gbDebug:
+                print("INFO:A:", la[4], la[5:])
         elif la[0] == "100":
             if state != 'A':
                 print("ERRR:ParseAptDat:{}:Runway data in wrong place?".format(lCnt))
                 continue
-            print("INFO:R:", la[9], la[10])
+            if gbDebug:
+                print("INFO:R:", la[9], la[10])
+            if not apt.get('lat'):
+                apt['lat'] = la[9]
+                apt['lon'] = la[10]
         else:
             continue
 
 
-parse_aptdat(sys.argv[1])
+gDB = {}
+parse_aptdat(sys.argv[1], gDB)
+print(gDB)
 
