0d6e8507399866f5503c63d7b4843e5c4de6a5f6
hanishkvc <hanishkvc@gmail.com>
2021-10-11 21:46:48 +0530

    Cleanup P3dPrims a bit

--- p3dprims.py
+++ p3dprims.py
@@ -24,13 +24,17 @@ class Polyhedron():
         self.gvwVertex.addData3(xyz)
         self.gvwTexCo.addData2(uv)
 
+    def add_vertices(self, va):
+        for v in va:
+            self.add_vertex(v)
+
     def add_triangle(self, vi0, vi1, vi2):
         self.prim.addVertices(vi0, vi1, vi2)
         self.prim.closePrimitive()
 
-    def add_triangles(self, va):
-        for v in va:
-            self.add_triangle(v[0], v[1], v[2])
+    def add_triangles(self, ta):
+        for t in ta:
+            self.add_triangle(t[0], t[1], t[2])
 
     def generate(self):
         self.geom = Geom(self.vdata)
@@ -39,23 +43,27 @@ class Polyhedron():
         self.geomNode.addGeom(self.geom)
         return self.geomNode
 
+
 def create_cube(name):
     m1 = Polyhedron(name)
-    m1.add_vertex((0,0,0))
-    m1.add_vertex((0,1,0))
-    m1.add_vertex((1,1,0))
-    m1.add_vertex((1,0,0))
-    m1.add_vertex((0,0,1))
-    m1.add_vertex((0,1,1))
-    m1.add_vertex((1,1,1))
-    m1.add_vertex((1,0,1))
-    va = [ [0,2,1], [0,3,2],
+    # Vertices array
+    va = [ (0,0,0),
+           (0,1,0),
+           (1,1,0),
+           (1,0,0),
+           (0,0,1),
+           (0,1,1),
+           (1,1,1),
+           (1,0,1) ]
+    m1.add_vertices(va)
+    # Triangle Faces array
+    ta = [ [0,2,1], [0,3,2],
            [4,6,5], [4,7,6],
            [0,4,5], [0,5,1],
            [3,7,6], [3,6,2],
            [0,3,7], [0,7,4],
            [1,2,6], [1,6,5] ]
-    m1.add_triangles(va)
+    m1.add_triangles(ta)
     m1gn = m1.generate()
     return m1gn
 
