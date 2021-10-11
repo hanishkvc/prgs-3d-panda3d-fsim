bf7296939957739b122de9e621b3392faab1cbe3
hanishkvc <hanishkvc@gmail.com>
2021-10-11 23:27:32 +0530

    Utils:Apt: Skeleton to parse apt.dat file
    Cleanup the lines
    
    Try extract the Airport ICAO code and Name,
    as well as runway lat lon.

---
+++ utils/hkvc-apt.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Work on apt.dat from FlightGear/XPlane
+# HanishKVC, 2021
+# GPL
+
+
+import sys
+
+
+def cleanup_line(sL):
+    la = []
+    prev = ''
+    for cur in sL:
+        if cur == ' ':
+            if prev == '':
+                continue
+            else:
+                la.append(prev)
+                prev = ''
+        else:
+            prev += cur
+    if prev != '':
+        la.append(prev)
+    return la
+
+
+def parse_aptdat(sFName):
+    f = open(sFName)
+    state = '0'
+    lCnt = 0
+    for l in f:
+        lCnt += 1
+        l = l.strip()
+        la = cleanup_line(l)
+        if len(la) == 0:
+            state = '0'
+            lat = None
+            lon = None
+            continue
+        else:
+            print("DBUG:",la)
+        if la[0] == "1":
+            if state != '0':
+                print("ParseAptDat:{}:New Airport Line before ending of prev airport data?".format(lCnt))
+                continue
+            state = 'A'
+            print("INFO:A:", la[4], la[5:])
+        elif la[0] == "100":
+            if state != 'A':
+                print("ParseAptDat:{}:Runway data in wrong place?".format(lCnt))
+                continue
+            print("INFO:R:", la[9], la[10])
+        else:
+            continue
+
+
+parse_aptdat(sys.argv[1])
+
